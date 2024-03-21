01db283f19806f2422f3346be31a9fd0b8e202e8
Cyrilloche <che.cyril@hotmail.fr>
2024-03-21 19:09:46 +0100

    Get all documents from the userId

--- JS/home.js
+++ JS/home.js
@@ -17,65 +17,70 @@ function returnUserId() {
     return userId;
 }
 
-// Fonction pour récupérer les données de l'utilisateur et les afficher
-function fetchUserData() {
-    const token = getTokenFromLocalStorage(); // Récupérer le token
-    const userId = returnUserId();
+// Fonction pour récupérer les données utilisateur
+async function fetchUserData() {
+    try {
+        const token = getTokenFromLocalStorage(); // Récupérer le token depuis le stockage local
+        const userId = returnUserId(); // Récupérer l'identifiant de l'utilisateur
 
-    fetch(`http://localhost:5037/api/User/${userId}`, {
+        const response = await fetch(`http://localhost:5037/api/User/${userId}`, {
             method: 'GET',
             headers: {
                 'Authorization': `Bearer ${token}`
             }
-        })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Error with datas get')
-            }
-            return response.json();
-        })
-        .then(userData => {
-            console.log(userData);
-            document.getElementById('welcomeUser').textContent = userData.userFirstname;
-        })
-        .catch(error => {
-            console.error('Error with user datas', error);
-        })
+        });
+
+        if (!response.ok) {
+            throw new Error('Erreur lors de la récupération des données utilisateur');
+        }
+
+        const userData = await response.json();
+        console.log(userData);
+        document.getElementById('welcomeUser').textContent = capitalizedFirstLetter(userData.userFirstname);
+    } catch (error) {
+        console.error('Erreur lors de la récupération des données utilisateur', error);
+    }
 }
 
-// Fonction pour récupérer les documents de l'utilisateur et les afficher
-function fetchUserPaper() {
-    const token = getTokenFromLocalStorage(); // Récupérer le token
-    const userId = returnUserId();
+// Fonction pour récupérer et afficher les documents
+async function fetchAndDisplayDocuments() {
+    try {
+        const token = getTokenFromLocalStorage();
+        const userId = returnUserId();
 
-    fetch(`http://localhost:5037/api/Paper/authorId?authorId=${userId}`, {
+        const response = await fetch(`http://localhost:5037/api/Paper/authorId?authorId=${userId}`, {
             method: 'GET',
             headers: {
                 'Authorization': `Bearer ${token}`
             }
-        })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Error with papers get')
-            }
-            return response.json();
-        })
-        .then(papers => {
-            console.log(papers);
-            addDocumentCardsToPage(papers); // Appel de la fonction pour afficher les documents
-        })
-        .catch(error => {
-            console.error('Error with user papers', error);
-        })
+        });
+
+        if (!response.ok) {
+            throw new Error('Erreur lors de la récupération des documents');
+        }
+
+        const papers = await response.json();
+        console.log(papers);
+
+        addDocumentCardsToPage(papers); // Ajouter les cartes de document à la page
+    } catch (error) {
+        console.error('Erreur lors de la récupération des documents', error);
+    }
+}
+
+// Appel des fonctions au chargement de la page
+window.onload = function () {
+    fetchUserData(); // Récupérer les données de l'utilisateur
+    fetchAndDisplayDocuments(); // Récupérer et afficher les documents de l'utilisateur
 }
 
 // Fonction pour créer dynamiquement un encart représentant un document
-function createDocumentCard(document) {
+function createDocumentCard(doc) {
     const documentCard = document.createElement('div');
     documentCard.classList.add('document-card');
 
-    const documentTitle = document.title;
-    const documentContent = document.content;
+    const documentTitle = doc.paperName;
+    const documentContent = doc.paperDescription;
 
     const titleElement = document.createElement('h3');
     titleElement.textContent = documentTitle;
@@ -92,15 +97,12 @@ function createDocumentCard(document) {
 function addDocumentCardsToPage(documents) {
     const documentsContainer = document.getElementById('documentsContainer');
 
-    documents.forEach(doc => { // Renommez 'document' en 'doc'
+    documents.forEach(doc => {
         const documentCard = createDocumentCard(doc);
         documentsContainer.appendChild(documentCard);
     });
 }
 
-
-// Appel des fonctions au chargement de la page
-window.onload = function () {
-    fetchUserData(); // Récupérer les données de l'utilisateur
-    fetchUserPaper(); // Récupérer les documents de l'utilisateur
+function capitalizedFirstLetter(word) {
+    return word.charAt(0).toUpperCase() + word.slice(1)
 }
